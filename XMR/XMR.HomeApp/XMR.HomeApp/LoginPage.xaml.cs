91a458c44f6533388e83113c285ba86426241553
valmon55 <fedor_ka@mail.ru>
2024-02-21 17:06:37 +0300

    попытки входа

--- XMR/XMR.HomeApp/XMR.HomeApp/LoginPage.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/LoginPage.xaml.cs
@@ -12,6 +12,8 @@ namespace XMR.HomeApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        public const string BUTTON_TEXT = "Войти";
+        public static int loginCounter = 0;
         public LoginPage()
         {
             InitializeComponent();
@@ -19,7 +21,26 @@ namespace XMR.HomeApp
 
         private void login_Click(object sender, EventArgs e)
         {
-            loginButton.Text = "Выполняется вход...";
+            if (loginCounter == 0)
+            {
+                // Если первая попытка - просто меняем сообщения
+                loginButton.Text = $"Выполняется вход..";
+            }
+            else if (loginCounter > 5) // Слишком много попыток - показываем ошибку
+            {
+                // Деактивируем кнопку
+                loginButton.IsEnabled = false;
+                // Показываем текстовое сообщение об ошибке
+                errorMessage.Text = "Слишком много попыток! Попробуйте позже.";
+            }
+            else
+            {
+                // Изменяем текст кнопки и показываем количество попыток входа
+                loginButton.Text = $"Выполняется вход...   Попыток входа: {loginCounter}";
+            }
+
+            // Увеличиваем счетчик
+            loginCounter += 1;
         }
     }
 }
