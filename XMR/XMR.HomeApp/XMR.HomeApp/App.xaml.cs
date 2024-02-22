7645dbcfd02b98d2255c6d6927c0eca65e86cfbb
valmon55 <fedor_ka@mail.ru>
2024-02-22 18:21:35 +0300

    ButtonViewExtention

--- XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XMR.HomeApp
         {
             InitializeComponent();
 
-            MainPage = new RoomsPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
