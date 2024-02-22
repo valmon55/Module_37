729e319ba59c530407d311aff32cb3ae8d46dff8
valmon55 <fedor_ka@mail.ru>
2024-02-22 18:49:04 +0300

    Вопрос

---
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/ColorExtention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Xamarin.Forms.Xaml;
+
+namespace XMR.HomeApp.Pages
+{
+    public class ColorExtention : IMarkupExtension
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Color.FromArgb(Red, Green, Blue);
+        }
+    }
+}
