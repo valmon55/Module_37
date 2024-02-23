dc4a44a7a25bfe98c4cf1ae199f70c3e3bafca2c
valmon55 <fedor_ka@mail.ru>
2024-02-23 19:44:04 +0300

    объединение ячеек в XAML

--- XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XMR.HomeApp
         {
             InitializeComponent();
 
-            MainPage = new GridPage();
+            MainPage = new MergeGridPage();
         }
 
         protected override void OnStart()
