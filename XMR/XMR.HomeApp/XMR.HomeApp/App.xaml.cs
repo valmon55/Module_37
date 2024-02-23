9e778585a3971b6ed1f23091ba14b64e0f2ffb8e
valmon55 <fedor_ka@mail.ru>
2024-02-23 16:05:59 +0300

    relativeLayout 1 element

--- XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XMR.HomeApp
         {
             InitializeComponent();
 
-            MainPage = new ClimatePage();
+            MainPage = new AboutPage();
         }
 
         protected override void OnStart()
