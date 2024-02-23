dc4f6d0df31442f6a029b1d93787c07d1be7d473
valmon55 <fedor_ka@mail.ru>
2024-02-23 19:17:38 +0300

    объединение ячеек

--- XMR/XMR.HomeApp/XMR.HomeApp/Pages/GridPage.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/GridPage.xaml.cs
@@ -28,23 +28,44 @@ namespace XMR.HomeApp.Pages
                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
                }
             };
+            PopulateGrid(grid);
+            //// Добавление элементов по определенным позициям
+            //grid.Children.Add(new BoxView { Color = Color.FromRgb(250, 253, 255) }, 0 /* Позиция слева */, 0 /* Позиция сверху */);
+            //grid.Children.Add(new BoxView { Color = Color.FromRgb(196, 232, 255) }, 0, 1);
+            //grid.Children.Add(new BoxView { Color = Color.FromRgb(133, 207, 255) }, 0, 2);
 
-            // Добавление элементов по определенным позициям
-            grid.Children.Add(new BoxView { Color = Color.FromRgb(250, 253, 255) }, 0 /* Позиция слева */, 0 /* Позиция сверху */);
-            grid.Children.Add(new BoxView { Color = Color.FromRgb(196, 232, 255) }, 0, 1);
-            grid.Children.Add(new BoxView { Color = Color.FromRgb(133, 207, 255) }, 0, 2);
+            //grid.Children.Add(new BoxView { Color = Color.FromRgb(87, 189, 255) }, 1, 0);
+            //grid.Children.Add(new BoxView { Color = Color.FromRgb(43, 172, 255) }, 1, 1);
+            //grid.Children.Add(new BoxView { Color = Color.FromRgb(23, 164, 255) }, 1, 2);
 
-            grid.Children.Add(new BoxView { Color = Color.FromRgb(87, 189, 255) }, 1, 0);
-            grid.Children.Add(new BoxView { Color = Color.FromRgb(43, 172, 255) }, 1, 1);
-            grid.Children.Add(new BoxView { Color = Color.FromRgb(23, 164, 255) }, 1, 2);
-
-            grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 121, 199) }, 2, 0);
-            grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 76, 199) }, 2, 1);
-            grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 76, 199) }, 2, 2);
+            //grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 121, 199) }, 2, 0);
+            //grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 76, 199) }, 2, 1);
+            //grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 76, 199) }, 2, 2);
 
             grid.ColumnSpacing = 20;
             grid.RowSpacing = 20;
             Content = grid;
+        }
+        public void PopulateGrid(Grid grid)
+        {
+            // Добавление элементов по определенным позициям
+            grid.Children.Add(new BoxView { Color = Color.FromRgb(250, 253, 255) }, 0 /* Позиция слева */, 0 /* Позиция сверху */);
+            // Сохраняем элемент перед добавлением, чтобы потом модифицировать
+            var mergedRow = new BoxView { Color = Color.FromRgb(196, 232, 255) };
+            // Добавляем в Grid
+            grid.Children.Add(mergedRow, 0, 1);
+            // Устанавливаем свойство ColumnSpan, чтобы расширить элемент на 3 позиции
+            Grid.SetColumnSpan(mergedRow, 3);
+
+            grid.Children.Add(new BoxView { Color = Color.FromRgb(133, 207, 255) }, 0, 2);
+
+            grid.Children.Add(new BoxView { Color = Color.FromRgb(87, 189, 255) }, 1, 0);
+            // grid.Children.Add(new BoxView { Color = Color.FromRgb(43, 172, 255) }, 1, 1);
+            grid.Children.Add(new BoxView { Color = Color.FromRgb(23, 164, 255) }, 1, 2);
+
+            grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 121, 199) }, 2, 0);
+            // grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 76, 199) }, 2, 1);
+            grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 76, 199) }, 2, 2);
         }
     }
 }
