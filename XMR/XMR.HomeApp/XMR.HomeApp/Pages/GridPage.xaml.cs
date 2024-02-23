f78f3bf8d56eeacd5ef24c2025239d66bc0a0143
valmon55 <fedor_ka@mail.ru>
2024-02-23 19:39:01 +0300

    объединени ячеек

--- XMR/XMR.HomeApp/XMR.HomeApp/Pages/GridPage.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/GridPage.xaml.cs
@@ -28,7 +28,9 @@ namespace XMR.HomeApp.Pages
                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
                }
             };
-            PopulateGrid(grid);
+            //PopulateGrid(grid);
+            MergeColumns(grid);
+
             //// Добавление элементов по определенным позициям
             //grid.Children.Add(new BoxView { Color = Color.FromRgb(250, 253, 255) }, 0 /* Позиция слева */, 0 /* Позиция сверху */);
             //grid.Children.Add(new BoxView { Color = Color.FromRgb(196, 232, 255) }, 0, 1);
@@ -66,6 +68,21 @@ namespace XMR.HomeApp.Pages
             grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 121, 199) }, 2, 0);
             // grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 76, 199) }, 2, 1);
             grid.Children.Add(new BoxView { Color = Color.FromRgb(0, 76, 199) }, 2, 2);
+        }
+        public void MergeColumns(Grid grid)
+        {
+            var mergeColumn1 = new BoxView { Color = Color.FromRgb(25, 253, 255) };
+            grid.Children.Add(mergeColumn1, 0, 0);
+            Grid.SetRowSpan(mergeColumn1, 3);
+
+            var mergeColumn3 = new BoxView { Color = Color.FromRgb(133, 207, 255) };
+            grid.Children.Add(mergeColumn3, 2, 0);
+            Grid.SetRowSpan(mergeColumn3, 3);
+
+            grid.Children.Add(new BoxView { Color = Color.FromRgb(87, 189, 255) }, 1, 0);
+            grid.Children.Add(new BoxView { Color = Color.FromRgb(43, 172, 255) }, 1, 1);
+            grid.Children.Add(new BoxView { Color = Color.FromRgb(23, 164, 255) }, 1, 2);
+
         }
     }
 }
