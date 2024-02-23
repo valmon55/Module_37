8cce445d44d80dc9f257dbdf84968445e1a33a28
valmon55 <fedor_ka@mail.ru>
2024-02-23 15:34:02 +0300

    absoluteLayout

--- XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XMR.HomeApp
         {
             InitializeComponent();
 
-            MainPage = new DevicePage();
+            MainPage = new ClimatePage();
         }
 
         protected override void OnStart()
---
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/ClimatePage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XMR.HomeApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ClimatePage : ContentPage
+    {
+        public ClimatePage()
+        {
+            InitializeComponent();
+            ScanInside();
+            ScanOutside();
+        }
+        /// <summary>
+        /// Внешние датчики
+        /// </summary>
+        public void ScanOutside()
+        {
+            absLayout.Children.Add(
+                // Создаем прямоугольник заданного цвета
+                new BoxView { Color = Color.LightBlue },
+                // Задаем его местоположение и размеры
+                new Rectangle(
+                    20, // X - координата начальной точки
+                    10, // Y - координата начальной точки
+                    100, // ширина прямоугольника
+                    70 // высота
+                )
+            );
+
+            absLayout.Children.Add(
+                new Label
+                {
+                    Text = $"Outside",
+                    VerticalTextAlignment = TextAlignment.Start,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    FontSize = 13
+                },
+                new Rectangle(20, 17, 100, 70)
+            );
+
+            absLayout.Children.Add(
+                new Label
+                {
+                    Text = "- 15 °C",
+                    VerticalTextAlignment = TextAlignment.Center,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    FontSize = 20
+                },
+                new Rectangle(20, 15, 100, 70)
+            );
+        }
+
+        /// <summary>
+        /// Внутренние датчики
+        /// </summary>
+        public void ScanInside()
+        {
+            absLayout.Children.Add(
+                new BoxView { Color = Color.LightSalmon },
+                new Rectangle(130, 10, 100, 70)
+            );
+
+            absLayout.Children.Add(
+                new Label
+                {
+                    Text = $"Inside",
+                    VerticalTextAlignment = TextAlignment.Start,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    FontSize = 13
+                },
+                new Rectangle(130, 17, 100, 70)
+            );
+
+            absLayout.Children.Add(
+                new Label
+                {
+                    Text = "+ 24 °C",
+                    VerticalTextAlignment = TextAlignment.Center,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    FontSize = 20
+                },
+                new Rectangle(130, 15, 100, 70)
+            );
+        }
+    }
+}
